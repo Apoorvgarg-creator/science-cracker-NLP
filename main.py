59766fcd3a27dd85c2f73f7ca35581a1b425036e
Apoorvgarg-Creator <57873504+Apoorvgarg-creator@users.noreply.github.com>
2024-04-04 14:29:26 +0530

    New: Hugging face Code Changes

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os
 import warnings
 from typing import Dict
 
+import torch
+from transformers import pipeline
+
+
+# create pieline for QA
 from openfabric_pysdk.utility import SchemaUtil
 
 from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
@@ -23,9 +28,23 @@ def config(configuration: Dict[str, ConfigClass], state: State):
 ############################################################
 def execute(request: SimpleText, ray: Ray, state: State) -> SimpleText:
     output = []
+    print(request)
     for text in request.text:
-        # TODO Add code here
-        response = 'Hello!'
+        print(text)
+        qa_response = answer(text)
+        print(qa_response)
+        response = qa_response
         output.append(response)
 
     return SchemaUtil.create(SimpleText(), dict(text=output))
+
+
+def answer(question):
+    hf = pipeline(
+        model="EleutherAI/gpt-neo-2.7B",
+        task="text-generation",
+        max_length=200,
+        pad_token_id=50256
+    )
+    response = hf(question)
+    return response[0]['generated_text']
